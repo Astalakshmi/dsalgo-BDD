f1519c834193e53090c6efce77b3000c6b9cc091
Sandhya Sankaran <sandhya@Sandhyas-MBP.attlocal.net>
2024-09-18 11:23:09 -0700

    added stepdefinitions for linked list and stack

---
+++ src/test/java/stepdefinitions/LinkedListStep.java
@@ -0,0 +1,205 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LinkedListStep {
+	
+	@Given("the user is in homepage and logged in")
+	public void the_user_is_in_homepage_and_logged_in() {
+	    
+	}
+
+	@When("the user clicks the get started button under linked list section")
+	public void the_user_clicks_the_get_started_button_under_linked_list_section() {
+	    
+	}
+
+	@Then("the user views the linked list page")
+	public void the_user_views_the_linked_list_page() {
+	    
+	}
+
+	@Given("the user is in linked list page")
+	public void the_user_is_in_linked_list_page() {
+	    
+	}
+
+	@When("the user clicks the Introduction link under the topics covered section")
+	public void the_user_clicks_the_introduction_link_under_the_topics_covered_section() {
+	    
+	}
+
+	@Then("the user views the Introduction page")
+	public void the_user_views_the_introduction_page() {
+	    
+	}
+
+	@Given("the User is on the Introduction page")
+	public void the_user_is_on_the_introduction_page() {
+	    
+	}
+
+	@When("the User clicks the Try here in Introduction Page")
+	public void the_user_clicks_the_try_here_in_introduction_page() {
+	    
+	}
+
+	@Then("the User should get navigated to the Try Editor Page")
+	public void the_user_should_get_navigated_to_the_try_editor_page() {
+	    
+	}
+
+	@Given("the User is on the Try Editor page")
+	public void the_user_is_on_the_try_editor_page() {
+	    
+	}
+
+	@When("the User enters a sample code as {string} in the Editor section and click Run")
+	public void the_user_enters_a_sample_code_as_in_the_editor_section_and_click_run(String string) {
+	    
+	}
+
+	@Then("the User should get the output in the screen")
+	public void the_user_should_get_the_output_in_the_screen() {
+	    
+	}
+
+	@When("user clicks the creating linked list link under the topics covered section")
+	public void user_clicks_the_creating_linked_list_link_under_the_topics_covered_section() {
+	    
+	}
+
+	@Then("user views the creating linked list page")
+	public void user_views_the_creating_linked_list_page() {
+	    
+	}
+
+	@Given("the User is on the Creating Linked list page")
+	public void the_user_is_on_the_creating_linked_list_page() {
+	    
+	}
+
+	@When("the User clicks the Try here in Creating Linked list Page")
+	public void the_user_clicks_the_try_here_in_creating_linked_list_page() {
+	    
+	}
+
+	@When("the User enters a sample code {string} in the Editor section and click Run")
+	public void the_user_enters_a_sample_code_in_the_editor_section_and_click_run(String string) {
+	    
+	}
+
+	@When("user clicks the Types of linked list link under the topics covered section")
+	public void user_clicks_the_types_of_linked_list_link_under_the_topics_covered_section() {
+	    
+	}
+
+	@Then("user views the types of linked list page")
+	public void user_views_the_types_of_linked_list_page() {
+	    
+	}
+
+	@Given("the User is on the Types of Linked List page")
+	public void the_user_is_on_the_types_of_linked_list_page() {
+	    
+	}
+
+	@When("the User clicks the Try here in Types of Linked List Page")
+	public void the_user_clicks_the_try_here_in_types_of_linked_list_page() {
+	    
+	}
+
+	@When("user clicks the implement linked list in python  link under the topics covered section")
+	public void user_clicks_the_implement_linked_list_in_python_link_under_the_topics_covered_section() {
+	    
+	}
+
+	@Then("user views the implement linked list in python  page")
+	public void user_views_the_implement_linked_list_in_python_page() {
+	    
+	}
+
+	@Given("the User is on the Implement linked list in python page")
+	public void the_user_is_on_the_implement_linked_list_in_python_page() {
+	    
+	}
+
+	@When("the User clicks the Try here in Implement linked list in python Page")
+	public void the_user_clicks_the_try_here_in_implement_linked_list_in_python_page() {
+	    
+	}
+
+	@When("user clicks the traversal link under the topics covered section")
+	public void user_clicks_the_traversal_link_under_the_topics_covered_section() {
+	    
+	}
+
+	@Then("user views the traversal page")
+	public void user_views_the_traversal_page() {
+	    
+	}
+
+	@Given("the User is on the Traversal page")
+	public void the_user_is_on_the_traversal_page() {
+	    
+	}
+
+	@When("the User clicks the Try here in Traversal Page")
+	public void the_user_clicks_the_try_here_in_traversal_page() {
+	    
+	}
+
+	@When("user clicks the insertion link under the topics covered section")
+	public void user_clicks_the_insertion_link_under_the_topics_covered_section() {
+	    
+	}
+
+	@Then("user views the insertion page")
+	public void user_views_the_insertion_page() {
+	    
+	}
+
+	@Given("the User is on the Insertion page")
+	public void the_user_is_on_the_insertion_page() {
+	    
+	}
+
+	@When("the User clicks the Try here in Insertion Page")
+	public void the_user_clicks_the_try_here_in_insertion_page() {
+	    
+	}
+
+	@When("user clicks the deletion link under the topics covered section")
+	public void user_clicks_the_deletion_link_under_the_topics_covered_section() {
+	    
+	}
+
+	@Then("user views the deletion page")
+	public void user_views_the_deletion_page() {
+	    
+	}
+
+	@Given("the User is on the Deletion page")
+	public void the_user_is_on_the_deletion_page() {
+	    
+	}
+
+	@When("the User clicks the Try here in Deletion Page")
+	public void the_user_clicks_the_try_here_in_deletion_page() {
+	    
+	}
+
+	@When("the User clicks the Practice Questions link in Introduction Page")
+	public void the_user_clicks_the_practice_questions_link_in_introduction_page() {
+	    
+	}
+
+	@Then("the User should get navigated to the Linked List Practice Questions Page")
+	public void the_user_should_get_navigated_to_the_linked_list_practice_questions_page() {
+	    
+	}
+
+
+}
---
+++ src/test/java/stepdefinitions/StackStep.java
@@ -0,0 +1,105 @@
+package stepdefinitions;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StackStep {
+	
+
+	
+	@Given("the user is in homepage")
+	public void the_user_is_in_homepage() {
+	    
+	}
+
+	@When("user clicks the get started button under stack section")
+	public void user_clicks_the_get_started_button_under_stack_section() {
+	    
+	}
+
+	@Then("user views the stack page")
+	public void user_views_the_stack_page() {
+	    
+	}
+
+	@Given("the user is in stack page")
+	public void the_user_is_in_stack_page() {
+	    
+	}
+
+	@When("user clicks the Operations in Stack link under the topics covered section")
+	public void user_clicks_the_operations_in_stack_link_under_the_topics_covered_section() {
+	    
+	}
+
+	@Then("user views the Operations in stack page")
+	public void user_views_the_operations_in_stack_page() {
+	    
+	}
+
+	@Given("the User is on the Operations in stack page")
+	public void the_user_is_on_the_operations_in_stack_page() {
+	    
+	}
+
+	@When("the User clicks the Try here in Operations in stack Page")
+	public void the_user_clicks_the_try_here_in_operations_in_stack_page() {
+	    
+	}
+
+	@When("user clicks the Implementation in Stack link under the topics covered section")
+	public void user_clicks_the_implementation_in_stack_link_under_the_topics_covered_section() {
+	    
+	}
+
+	@Then("user views the Implementation in stack page")
+	public void user_views_the_implementation_in_stack_page() {
+	    
+	}
+
+	@Given("the User is on the Implementation in stack page")
+	public void the_user_is_on_the_implementation_in_stack_page() {
+	    
+	}
+
+	@When("the User clicks the Try here in Implementation in stack Page")
+	public void the_user_clicks_the_try_here_in_implementation_in_stack_page() {
+	    
+	}
+
+	@When("user clicks the Applications in Stack link under the topics covered section")
+	public void user_clicks_the_applications_in_stack_link_under_the_topics_covered_section() {
+	    
+	}
+
+	@Then("user views the Applications in stack page")
+	public void user_views_the_applications_in_stack_page() {
+	    
+	}
+
+	@Given("the User is on the Applications in stack page")
+	public void the_user_is_on_the_applications_in_stack_page() {
+	    
+	}
+
+	@When("the User clicks the Try here in Applications in stack Page")
+	public void the_user_clicks_the_try_here_in_applications_in_stack_page() {
+	    
+	}
+
+	@When("the User clicks the Practice Questions link in Operations in stack Page")
+	public void the_user_clicks_the_practice_questions_link_in_operations_in_stack_page() {
+	    
+	}
+
+	@Then("the User should get navigated to the Stack Practice Questions Page")
+	public void the_user_should_get_navigated_to_the_stack_practice_questions_page() {
+	    
+	}
+
+
+
+
+}
