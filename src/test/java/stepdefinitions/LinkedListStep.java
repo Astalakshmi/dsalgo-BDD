a144644ae6fe713ecc378d84eb01aa341f82a553
Sandhya Sankaran <sandhya@Sandhyas-MBP.attlocal.net>
2024-09-25 11:19:54 -0700

    added step defs for linked list practice

--- src/test/java/stepdefinitions/LinkedListStep.java
+++ src/test/java/stepdefinitions/LinkedListStep.java
@@ -20,47 +20,14 @@ import pageObjects.LinkedListPage;
 public class LinkedListStep {
 	
 	WebDriver driver =  WebdriverManager.getDriver();
-	LinkedListPage lp = new LinkedListPage();
+	LinkedListPage linkedListObject = new LinkedListPage();
 	ConfigFileReader configFileReader = WebdriverManager.configReader();
 	
 	
-//	
-//	@Given("The User navigates to the login page")
-//	public void the_user_navigates_to_the_login_page() {
-//		
-//		driver.get(configFileReader.getLoginUrl());
-//		LoggerLoad.info("You are in" +driver.getTitle());
-//	    
-//	}
-//
-//	@When("The User valid credentials and clicks the login button")
-//	public void the_user_valid_credentials_and_clicks_the_login_button() {
-//		lp.setUserName(configFileReader.getUsername());
-//		lp.setPassword(configFileReader.getPassword());
-//		lp.clickLoginBtn();
-//	    
-//	}
-//
-//	@Then("The User should be navigated to the Home page and logged in")
-//	public void the_user_should_be_navigated_to_the_home_page_and_logged_in() {
-//		
-//		String homePageTitle = driver.getTitle();
-//		LoggerLoad.info("You are in" +homePageTitle);
-//	    Assert.assertEquals("NumpyNinja", homePageTitle);
-//		
-//	    
-//	}
-//
-//	@Given("the user is in homepage and logged in")
-//	public void the_user_is_in_homepage_and_logged_in() {
-//		driver.get(configFileReader.getHomeUrl());
-//		LoggerLoad.info("You are in" +BrowserDriver.getTitle());
-//		
-//	}
 
 	@When("the user clicks the get started button under linked list section")
 	public void the_user_clicks_the_get_started_button_under_linked_list_section() {
-		lp.clickGetStartedBtn();
+		linkedListObject.clickGetStartedBtn();
 	    
 	}
 
@@ -77,14 +44,14 @@ public class LinkedListStep {
 	@Given("the user is in linked list page")
 	public void the_user_is_in_linked_list_page() {
 		
-		lp.clickGetStartedBtn();
+		linkedListObject.clickGetStartedBtn();
 		//LoggerLoad.info("You are in" +driver.getTitle());
 	    
 	}
 
 	@When("the user clicks the Introduction link under the topics covered section")
 	public void the_user_clicks_the_introduction_link_under_the_topics_covered_section() {
-	    lp.clickIntroductionLink();
+		linkedListObject.clickIntroductionLink();
 	}
 
 	@Then("the user views the Introduction page")
@@ -97,15 +64,15 @@ public class LinkedListStep {
 
 	@Given("the User is on the Introduction page")
 	public void the_user_is_on_the_introduction_page() {
-		lp.clickGetStartedBtn();
-		lp.clickIntroductionLink();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clickIntroductionLink();
 		//LoggerLoad.info("You are in" +driver.getTitle());
 	}
 
 	@When("the User clicks the Try here in Introduction Page")
 	public void the_user_clicks_the_try_here_in_introduction_page() {
 		
-		lp.clicktryEditor();
+		linkedListObject.clicktryEditor();
 	    
 	}
 
@@ -118,9 +85,9 @@ public class LinkedListStep {
 
 	@Given("the User is on the Try Editor page through the introduction page")
 	public void the_user_is_on_the_try_editor_page_through_the_introduction_page() {
-		lp.clickGetStartedBtn();
-		lp.clickIntroductionLink();
-		lp.clicktryEditor();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clickIntroductionLink();
+		linkedListObject.clicktryEditor();
 		//LoggerLoad.info("You are in" +driver.getTitle());
 		
 	    
@@ -158,7 +125,7 @@ public class LinkedListStep {
 	@When("user clicks the creating linked list link under the topics covered section")
 	public void user_clicks_the_creating_linked_list_link_under_the_topics_covered_section() {
 		
-		lp.clickCreatingLinkedListLink();
+		linkedListObject.clickCreatingLinkedListLink();
 	    
 	}
 
@@ -172,29 +139,29 @@ public class LinkedListStep {
 
 	@Given("the User is on the Creating Linked list page")
 	public void the_user_is_on_the_creating_linked_list_page() {
-		lp.clickGetStartedBtn();
-		lp.clickCreatingLinkedListLink();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clickCreatingLinkedListLink();
 	    
 	}
 
 	@When("the User clicks the Try here in Creating Linked list Page")
 	public void the_user_clicks_the_try_here_in_creating_linked_list_page() {
-		lp.clicktryEditor();
+		linkedListObject.clicktryEditor();
 	    
 	}
 
 	@Given("the User is on the Try Editor page through the creating linked list page")
 	public void the_user_is_on_the_try_editor_page_through_the_creating_linked_list_page() {
-		lp.clickGetStartedBtn();
-		lp.clickCreatingLinkedListLink();
-		lp.clicktryEditor();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clickCreatingLinkedListLink();
+		linkedListObject.clicktryEditor();
 	    
 	}
 
 	@When("user clicks the Types of linked list link under the topics covered section")
 	public void user_clicks_the_types_of_linked_list_link_under_the_topics_covered_section() {
 		
-		lp.clickTypesOfLinkedListLink();
+		linkedListObject.clickTypesOfLinkedListLink();
 	    
 	}
 
@@ -206,28 +173,28 @@ public class LinkedListStep {
 
 	@Given("the User is on the Types of Linked List page")
 	public void the_user_is_on_the_types_of_linked_list_page() {
-		lp.clickGetStartedBtn();
-		lp.clickTypesOfLinkedListLink();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clickTypesOfLinkedListLink();
 	    
 	}
 
 	@When("the User clicks the Try here in Types of Linked List Page")
 	public void the_user_clicks_the_try_here_in_types_of_linked_list_page() {
-		lp.clicktryEditor();
+		linkedListObject.clicktryEditor();
 	    
 	}
 	
 	@Given("the User is on the Try Editor page through the types of linkedlist page")
 	public void the_user_is_on_the_try_editor_page_through_the_types_of_linkedlist_page() {
-		lp.clickGetStartedBtn();
-		lp.clickTypesOfLinkedListLink();
-		lp.clicktryEditor();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clickTypesOfLinkedListLink();
+		linkedListObject.clicktryEditor();
 	    
 	}
 
 	@When("user clicks the implement linked list in python  link under the topics covered section")
 	public void user_clicks_the_implement_linked_list_in_python_link_under_the_topics_covered_section() {
-		lp.clickImplementLinkedListLink();
+		linkedListObject.clickImplementLinkedListLink();
 	    
 	}
 
@@ -239,28 +206,28 @@ public class LinkedListStep {
 
 	@Given("the User is on the Implement linked list in python page")
 	public void the_user_is_on_the_implement_linked_list_in_python_page() {
-		lp.clickGetStartedBtn();
-		lp.clickImplementLinkedListLink();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clickImplementLinkedListLink();
 	    
 	}
 
 	@When("the User clicks the Try here in Implement linked list in python Page")
 	public void the_user_clicks_the_try_here_in_implement_linked_list_in_python_page() {
-		lp.clicktryEditor();
+		linkedListObject.clicktryEditor();
 	    
 	}
 	
 	@Given("the User is on the Try Editor page through the implement linked list in python page")
 	public void the_user_is_on_the_try_editor_page_through_the_implement_linked_list_in_python_page() {
-		lp.clickGetStartedBtn();
-		lp.clickImplementLinkedListLink();
-		lp.clicktryEditor();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clickImplementLinkedListLink();
+		linkedListObject.clicktryEditor();
 	    
 	}
 
 	@When("user clicks the traversal link under the topics covered section")
 	public void user_clicks_the_traversal_link_under_the_topics_covered_section() {
-		lp.clicktraversalLink();
+		linkedListObject.clicktraversalLink();
 	    
 	}
 
@@ -272,29 +239,29 @@ public class LinkedListStep {
 
 	@Given("the User is on the Traversal page")
 	public void the_user_is_on_the_traversal_page() {
-		lp.clickGetStartedBtn();
-		lp.clicktraversalLink();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clicktraversalLink();
 	    
 	}
 
 	@When("the User clicks the Try here in Traversal Page")
 	public void the_user_clicks_the_try_here_in_traversal_page() {
-		lp.clicktryEditor();
+		linkedListObject.clicktryEditor();
 	    
 	}
 	
 	@Given("the User is on the Try Editor page through the traversal page")
 	public void the_user_is_on_the_try_editor_page_through_the_traversal_page() {
-		lp.clickGetStartedBtn();
-		lp.clicktraversalLink();
-		lp.clicktryEditor();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clicktraversalLink();
+		linkedListObject.clicktryEditor();
 	    
 	    
 	}
 
 	@When("user clicks the insertion link under the topics covered section")
 	public void user_clicks_the_insertion_link_under_the_topics_covered_section() {
-		lp.clickinsertionLink();
+		linkedListObject.clickinsertionLink();
 	    
 	}
 
@@ -306,28 +273,28 @@ public class LinkedListStep {
 
 	@Given("the User is on the Insertion page")
 	public void the_user_is_on_the_insertion_page() {
-		lp.clickGetStartedBtn();
-		lp.clickinsertionLink();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clickinsertionLink();
 	    
 	}
 
 	@When("the User clicks the Try here in Insertion Page")
 	public void the_user_clicks_the_try_here_in_insertion_page() {
-		lp.clicktryEditor();
+		linkedListObject.clicktryEditor();
 	    
 	}
 	
 	@Given("the User is on the Try Editor page through the insertion page")
 	public void the_user_is_on_the_try_editor_page_through_the_insertion_page() {
-		lp.clickGetStartedBtn();
-		lp.clickinsertionLink();
-		lp.clicktryEditor();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clickinsertionLink();
+		linkedListObject.clicktryEditor();
 	    
 	}
 
 	@When("user clicks the deletion link under the topics covered section")
 	public void user_clicks_the_deletion_link_under_the_topics_covered_section() {
-		lp.clickdeletionLink();
+		linkedListObject.clickdeletionLink();
 	    
 	}
 
@@ -339,30 +306,61 @@ public class LinkedListStep {
 
 	@Given("the User is on the Deletion page")
 	public void the_user_is_on_the_deletion_page() {
-		lp.clickGetStartedBtn();
-		lp.clickdeletionLink();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clickdeletionLink();
 	    
 	}
 
 	@When("the User clicks the Try here in Deletion Page")
 	public void the_user_clicks_the_try_here_in_deletion_page() {
-		lp.clicktryEditor();
+		linkedListObject.clicktryEditor();
 	    
 	}
 	
 	@Given("the User is on the Try Editor page through the deletion page")
 	public void the_user_is_on_the_try_editor_page_through_the_deletion_page() {
-		lp.clickGetStartedBtn();
-		lp.clickdeletionLink();
-		lp.clicktryEditor();
+		linkedListObject.clickGetStartedBtn();
+		linkedListObject.clickdeletionLink();
+		linkedListObject.clicktryEditor();
 	    
 	}
 
 
 	@When("the User clicks the Practice Questions link in Introduction Page")
 	public void the_user_clicks_the_practice_questions_link_in_introduction_page() {
-		lp.clickPracticeLink();
+		linkedListObject.clickPracticeLink();
 	    
+	}
+	
+	@When("the User clicks the Practice Questions link in Creating Linked list Page")
+	public void the_user_clicks_the_practice_questions_link_in_creating_linked_list_page() {
+		linkedListObject.clickPracticeLink();
+	}
+
+	@When("the User clicks the Practice Questions link in Implement linked list Page")
+	public void the_user_clicks_the_practice_questions_link_in_implement_linked_list_page() {
+		linkedListObject.clickPracticeLink();
+	}
+
+	@When("the User clicks the Practice Questions link in Types of Linked List Page")
+	public void the_user_clicks_the_practice_questions_link_in_types_of_linked_list_page() {
+		linkedListObject.clickPracticeLink();
+	    
+	}
+
+	@When("the User clicks the Practice Questions link in Traversal Page")
+	public void the_user_clicks_the_practice_questions_link_in_traversal_page() {
+		linkedListObject.clickPracticeLink();
+	}
+
+	@When("the User clicks the Practice Questions link in Insertion Page")
+	public void the_user_clicks_the_practice_questions_link_in_insertion_page() {
+		linkedListObject.clickPracticeLink();
+	}
+
+	@When("the User clicks the Practice Questions link in Deletion Page")
+	public void the_user_clicks_the_practice_questions_link_in_deletion_page() {
+		linkedListObject.clickPracticeLink();
 	}
 
 	@Then("the User should get navigated to the Linked List Practice Questions Page")
