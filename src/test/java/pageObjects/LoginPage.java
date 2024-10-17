d87a99a3a24a24722aa186addd38a5fd6804bfe6
Sandhya Sankaran <svsandhya23@gmail.com>
2024-10-16 22:44:17 -0700

    Added explicit wait for signin click

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -1,9 +1,13 @@
 package pageObjects;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import driverManager.WebdriverManager;
 
@@ -36,6 +40,8 @@ public class LoginPage {
 	WebElement registerLink;
 
 	public void signInclick() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(signIn)).click();
 		signIn.click();
 	}
 
